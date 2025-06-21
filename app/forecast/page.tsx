8b4f361211f123c83f144a655a9c53c4610ee2c1
sgsayani <sgsayanighatak@gmail.com>
2025-06-22 04:02:29 +0530

    6th commit

--- app/forecast/page.tsx
+++ app/forecast/page.tsx
@@ -1,3 +1,13 @@
+import { HourlyForecastHeader } from "../dashboard/_components/hourly-forecast-header";
+import { HourlyView } from "../dashboard/_components/hourly-view/hourly-view";
+
 export default function ForecastPage() {
-    return <div>Forecast</div>;
+    return (
+        <div className="flex flex-col gap-6">
+          
+          <HourlyForecastHeader/>
+          <HourlyView/>
+          
+        </div>
+      );
   }
